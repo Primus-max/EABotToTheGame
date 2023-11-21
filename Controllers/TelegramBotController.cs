aa3217a33c2dab9c1b7479df5befd57d23844e83
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-11-21 15:59:46 +0300

    Добавьте файлы проекта.

---
+++ Controllers/TelegramBotController.cs
@@ -0,0 +1,124 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace SexoPolisBot.Controllers
+{
+    /// <summary>
+    /// Базовый класс создающий бота и прослушивающий сообщения
+    /// </summary>
+    public class TelegramBotController
+    {
+        private readonly ITelegramBotClient _botClient;
+       
+        public TelegramBotController(ITelegramBotClient botClient)
+        {
+            _botClient = botClient;            
+        }
+
+
+        /// <summary>
+        /// Метод инициализации бота и его запуск. Метод-точка входа в приложение
+        /// </summary>
+        /// <returns></returns>
+        public async Task StartBotAsync()
+        {
+            using var cts = new CancellationTokenSource();
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            ReceiverOptions receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>(), // receive all update types except ChatMember related updates
+                ThrowPendingUpdates = true
+            };
+
+            await _botClient.ReceiveAsync(
+                 updateHandler: HandleUpdateAsync,
+                 pollingErrorHandler: HandlePollingErrorAsync,
+                 receiverOptions: receiverOptions,
+                 cancellationToken: cts.Token
+             );
+
+            var me = await _botClient.GetMeAsync();
+            Console.WriteLine($"Bot Id: {me.Id}, Bot Name: {me.Username}");
+        }
+
+        /// <summary>
+        /// Метод прослушивающий и обрабатывающий входящие сообщения от пользователей
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="update"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {           
+            // Если информация пришла из канала где бот администратор
+            if (update.ChannelPost != null || update.Type == UpdateType.ChatJoinRequest)
+            {
+                try
+                {
+                   // Task.Run(() => _channelPostHandlerManager.HandleChannelPostAsync(botClient, update, cancellationToken));
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            // Если пришло текстовое сообщение
+            if (update.Message != null)
+            {
+                try
+                {
+                   // Task.Run(() => _messageHandlerManager.HandleMessageAsync(botClient, update, cancellationToken));
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            // Если пришёл Callback
+            if (update.CallbackQuery != null)
+            {
+
+                try
+                {
+                   // Task.Run(() => _callbackQueryHandler.HandleCallbackQueryAsync(botClient, update, cancellationToken));
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+
+            }
+
+        }
+
+        /// <summary>
+        /// Метод для перехвата и логирования ошибок
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="exception"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EABotToTheGame
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            // Создание контейнера DI и регистрация зависимостей
+            var serviceProvider = new ServiceCollection()
+                
+                .AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6844658250:AAGPSNBy3Opa6zrW_5Yyn58JyFDpSdcoRg0"))
+                
+                .AddLogging()
+                .BuildServiceProvider();
+
+            
+
+            // Создание главного класса и вызов начального метода
+            var botController = serviceProvider.GetRequiredService<TelegramBotController>();
+            await botController.StartBotAsync();
+
+
+            Console.WriteLine($"НЕ нажимайте любую кнопку! Это остановит работу бота");
+            Console.ReadKey();
+
+        }
+    }
+}
